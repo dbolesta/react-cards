5c3e47b1ab50cc955830da12729ea719e02b3ddc
Damon Bolesta <dbolesta@gmail.com>
2019-05-05 17:09:59 -0400

    Created first iteration of Card component with styling for image background, arrows, and arrow numbers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Card.css';
+
+
+
+class Card extends React.Component {
+  render() {
+
+
+
+    return (
+      <div class="card">
+        <div class="arrow-container">
+          <span class="arrow arrow-top-left">
+            <span>10</span>
+          </span>
+
+          <span class="arrow arrow-top">
+            <span>9</span>
+          </span>
+
+          <span class="arrow arrow-top-right">
+            <span>8</span>
+          </span>
+
+          <span class="arrow arrow-left">
+            <span>4</span>
+          </span>
+
+          <span class="arrow arrow-right">
+            <span>4</span>
+          </span>
+
+          <span class="arrow arrow-bottom-left">
+            <span>10</span>
+          </span>
+
+          <span class="arrow arrow-bottom">
+            <span>10</span>
+          </span>
+
+          <span class="arrow arrow-bottom-right">
+            <span>4</span>
+          </span>
+
+        </div>
+
+      </div>
+    )
+
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Card />
+
     </div>
   );
 }
