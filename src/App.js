5269e0386035b2f153ffa0819c359ec12b903284
Damon Bolesta <dbolesta@gmail.com>
2019-06-21 16:35:16 -0400

    removed arrow numbers from arrow container on Card component so that animated arrows - stabbing - do not also animate the numbers. Adjusted number css position accordingly

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import Card from './Card'
-import CardHolder from './CardHolder'
-import Board from './Board';
-import './App.css';
-import './Card.css';
-import HTML5Backend from 'react-dnd-html5-backend'
-import { DragDropContext } from 'react-dnd'
-import allCards from './AllCards'
-import update from 'immutability-helper';
-import * as utils from './utils'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Card from "./Card";
+import CardHolder from "./CardHolder";
+import Board from "./Board";
+import "./App.css";
+import "./Card.css";
+import HTML5Backend from "react-dnd-html5-backend";
+import { DragDropContext } from "react-dnd";
+import allCards from "./AllCards";
+import update from "immutability-helper";
+import * as utils from "./utils";
 
-
-
-const gman = utils.getUniqueRandomArray(10);
-
+const randArr = utils.getUniqueRandomArray(10);
 
 class App extends Component {
   constructor(props) {
@@ -28,68 +25,72 @@ class App extends Component {
         [null, null, null, null]
       ],
       p1hand: [
-        gman[0], gman[1], gman[2], gman[3], gman[4]
+        randArr[0],
+        randArr[1],
+        randArr[2],
+        randArr[3],
+        randArr[4]
       ],
       p2hand: [
-        gman[6], gman[7], gman[8], gman[9], gman[10]
+        randArr[6],
+        randArr[7],
+        randArr[8],
+        randArr[9],
+        randArr[10]
       ],
       score: {
         p1: 9,
         p2: 123
       }
-    }
+    };
   }
-
 
   handlePlayCard(index, player, bxy, id) {
     console.log("State???");
-    console.log(this.state[player + "hand"][index])
+    console.log(this.state[player + "hand"][index]);
 
-    console.log("...move card " + id + " to " + bxy.x + "-" + bxy.y + "???");
+    console.log(
+      "...move card " + id + " to " + bxy.x + "-" + bxy.y + "???"
+    );
 
     let hand = player + "hand";
 
     this.setState({
-      [player + "hand"]: update(this.state[hand], { [index]: { $set: null } }),
-      board: update(this.state.board, { [bxy.x]: { [bxy.y]: { $set: { id, player } } } })
-    })
+      [player + "hand"]: update(this.state[hand], {
+        [index]: { $set: null }
+      }),
+      board: update(this.state.board, {
+        [bxy.x]: { [bxy.y]: { $set: { id, player } } }
+      })
+    });
 
     // this.setState({
     //   board: update(this.state.board, { [bxy.x]: { [bxy.y]: { $set: id } } })
     // })
-
   }
-
 
   render() {
     return (
-      <div className="App" >
-
+      <div className="App">
         <h1>Tetra Master</h1>
 
         <div className="play-area">
-
           <CardHolder
             player="p1"
             hand={this.state.p1hand}
             onPlayCard={this.handlePlayCard}
           />
-          <Board
-            score={this.state.score}
-            board={this.state.board}
-          />
+          <Board score={this.state.score} board={this.state.board} />
           <CardHolder
             player="p2"
             hand={this.state.p2hand}
             onPlayCard={this.handlePlayCard}
           />
-
         </div>
-
       </div>
     );
   }
 }
 
-export default DragDropContext(HTML5Backend)(App)
+export default DragDropContext(HTML5Backend)(App);
 //export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,75 +1,88 @@
-import React, { Component } from 'react'
-import { DragSource } from 'react-dnd'
-
+import React, { Component } from "react";
+import { DragSource } from "react-dnd";
 
 const itemSource = {
-   beginDrag(props) {
-      console.log("Im draggin ma");
-      return props;
-   },
-   endDrag(props, monitor, component) {
-      if (!monitor.didDrop()) {
-         return;
-      }
+  beginDrag(props) {
+    console.log("Im draggin ma");
+    return props;
+  },
+  endDrag(props, monitor, component) {
+    if (!monitor.didDrop()) {
+      return;
+    }
 
-      console.log("TESTING END DRAG PASS???");
-      console.log(monitor.getDropResult());
+    console.log("TESTING END DRAG PASS???");
+    console.log(monitor.getDropResult());
 
-      return props.onDrop(props.index, props.cardData.owner, monitor.getDropResult(), props.cardData.id);
-   },
-   canDrag(props) { // used to disable dragging if card is on the Board ("inPlay")
-      if (props.inPlay) {
-         return false
-      } else {
-         return true
-      }
-   }
-}
+    return props.onDrop(
+      props.index,
+      props.cardData.owner,
+      monitor.getDropResult(),
+      props.cardData.id
+    );
+  },
+  canDrag(props) {
+    // used to disable dragging if card is on the Board ("inPlay")
+    if (props.inPlay) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
 
 function collect(connect, monitor) {
-   return {
-      connectDragSource: connect.dragSource(),
-      connectDragPreview: connect.dragPreview(),
-      isDragging: monitor.isDragging()
-   }
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging()
+  };
 }
 
 class Card extends Component {
-   render() {
-      const { isDragging, connectDragSource } = this.props;
-      const { numbers, title, owner } = this.props.cardData;
+  render() {
+    const { isDragging, connectDragSource } = this.props;
+    const { numbers, title, owner } = this.props.cardData;
 
+    const opacity = isDragging ? 0 : 1;
+    // container for all arrow JSX
+    const arrows = [];
 
-      const opacity = isDragging ? 0 : 1;
-      // container for all arrow JSX
-      const arrows = [];
+    // loop through numbers object prop using Object.keys so we can get key / value
+    Object.keys(numbers).map(function(keyName, keyIndex) {
+      // if no number, return empty handed
+      if (numbers[keyName] === null) return;
 
-      // loop through numbers object prop using Object.keys so we can get key / value
-      Object.keys(numbers).map(function (keyName, keyIndex) {
+      // add the JSX using keyName for styling
+      arrows.push(
+        <React.Fragment>
+          <span className={"arrow arrow-" + keyName} key={keyName}>
+            {/* <span>{numbers[keyName]}</span> */}
+          </span>
+          <span
+            className={"arrow-num arrow-" + keyName + "-num"}
+            key={keyName + "-num"}
+          >
+            {numbers[keyName]}
+          </span>
+        </React.Fragment>
+      );
+    });
 
-         // if no number, return empty handed
-         if (numbers[keyName] === null) return;
-
-         // add the JSX using keyName for styling
-         arrows.push(
-            <span className={"arrow arrow-" + keyName} key={keyName}>
-               <span>{numbers[keyName]}</span>
-            </span>
-         )
-
-      })
-
-      return connectDragSource(
-         <div className={"card" + " card-" + owner} style={{ opacity }} title={title}>
-            <div className="arrow-container">
-               {arrows}
-               <div className="card-color"></div>
-            </div>
-         </ div>
-      )
-
-   }
+    return connectDragSource(
+      <div
+        className={"card" + " card-" + owner}
+        style={{ opacity }}
+        title={title}
+      >
+        <div className="arrow-container">
+          {arrows}
+          <div className="card-color" />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default DragSource('card', itemSource, collect)(Card)
+export default DragSource("card", itemSource, collect)(Card);
 //export default Card
